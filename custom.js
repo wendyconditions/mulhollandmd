4040c6baf7f1d713feb07c5f5c8b4d2d0451a843
Wendy <wendymaldonado.la@gmail.com>
2020-03-04 20:35:41 -0800

    minor changes css and animation flow

--- custom.js
+++ custom.js
@@ -1,5 +1,6 @@
 $(function(){
     $( document ).ready(function() {
-        $("#main_h1").removeClass("d-none").addClass("animated slideInLeft");
+        $("#main_h1").removeClass("invisible").addClass("animated slideInLeft");
+        //$( "#main_h1" ).data( "data-wow-duration", "1.5s" );
     });
 });
