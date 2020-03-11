1681c755c460ae375e73c522bd8290548289f25d
Wendy Maldonado <wam922@gmail.com>
2020-03-11 15:32:28 -0700

    clean up

--- custom.js
+++ custom.js
@@ -1,11 +1,5 @@
 $(function(){
     $( document ).ready(function() {
         $("#wm_main_h1").removeClass("invisible").addClass("animated slideInLeft");
-
-        $('.collapse').on('shown.bs.collapse', function(){
-            //console.log('hi');
-        })
-
-       // document.addEventListener('collapse', 'shown.bs.collapse', {passive: false});
     });
 });
